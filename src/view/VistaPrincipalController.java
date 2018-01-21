74c837851b8fc79f9bc42e520eb25d7167eb166f
calata98 <pabloo-c-1998@hotmail.com>
2018-01-21 15:03:18 +0100

    Añadido VistaPersonaController pero está sin acabar

---
+++ src/view/VistaPrincipalController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.GestorLibreria;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ *
+ * @author calata98
+ */
+public class VistaPrincipalController {
+    
+    @FXML
+    private TableView tablaP;
+    
+    @FXML
+    private TableColumn columID;
+    
+    @FXML
+    private TableColumn columN;
+    
+    
+    private GestorLibreria gestorLibreria;
+    
+    VistaPrincipalController(){
+        
+    }
+    
+    @FXML
+    private void initialize() {
+        
+        
+        String ID = "SUPA";
+        String nombre = "DUPA";
+        columID.setCellValueFactory(new PropertyValueFactory<>(ID));
+        columN.setCellValueFactory(new PropertyValueFactory<>(nombre));
+    }
+    
+    public void setLibretaDirecciones(GestorLibreria gestorLibreria) {
+        
+        this.gestorLibreria = gestorLibreria;
+    }
+
+    
+}
