6ab27013f30994655fb05f692db7e69e067b73ca
calata98 <pabloo-c-1998@hotmail.com>
2018-01-30 00:25:35 +0100

    Añadido libro y producto bien, y creando la vista de añadir producto(libro)

--- src/model/Libro.java
+++ src/model/Libro.java
@@ -13,8 +13,59 @@ import java.util.Date;
  */
 public class Libro extends Producto {
     
+    //el ISBN son 13 digitos***********
+    
+    long ISBN;
+    String genero;
+    String autor;
+    String editorial;
+    Date anoPublicacion;
+    
+     public Libro(long ISBN, String genero, String autor, Date anoPublicacion, String editorial, String foto, String nombre, String description, double precio, int stock, int codBarras, Date fechaAlta, Date fechaModificacion) {
+        super(foto, nombre, description, precio, stock, codBarras, fechaAlta, fechaModificacion);
+        this.ISBN = ISBN;
+        this.genero = genero;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion;
+        this.editorial=editorial;
+    }
+    
     public Libro(String foto, String nombre, String description, double precio, int stock, int codBarras, Date fechaAlta, Date fechaModificacion) {
         super(foto, nombre, description, precio, stock, codBarras, fechaAlta, fechaModificacion);
     }
+
+    public long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getAnoPublicacion() {
+        return anoPublicacion;
+    }
+
+    public void setAnoPublicacion(Date anoPublicacion) {
+        this.anoPublicacion = anoPublicacion;
+    }
+
+   
     
 }
