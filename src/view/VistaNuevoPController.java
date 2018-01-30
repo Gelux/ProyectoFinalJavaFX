a254658ce427ea701d6ed95696220e91fd1b5023
calata98 <pabloo-c-1998@hotmail.com>
2018-01-30 13:15:17 +0100

    Casi terminada la vista de añadir libro y creado el controller.

---
+++ src/view/VistaNuevoPController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import controller.GestorLibreria;
+
+/**
+ *
+ * @author calata98
+ */
+public class VistaNuevoPController {
+    private GestorLibreria gestorLibreria;
+    
+    
+    public void setGestorLibreria(GestorLibreria gestorLibreria) {
+        
+        this.gestorLibreria = gestorLibreria;
+    }
+}
