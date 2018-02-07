fb7e6421618411c6f102c1be08c5116a6acd19cc
Gelux <geluxedeluxe@gmail.com>
2018-02-07 20:57:03 +0100

    Añadida clase y metodo para listar los productos de la database y cambiados los controllers y views de listado

--- src/util/DatabaseUtil.java
+++ src/util/DatabaseUtil.java
@@ -3,12 +3,16 @@ package util;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import model.Libro;
 import model.Producto;
 
 /**
@@ -16,18 +20,18 @@ import model.Producto;
  * @author Jes
  */
 public class DatabaseUtil {
-    
+
     Producto productoAux = null;
     Connection conexion = null;
     Statement sentencia = null;
     ResultSet resultSet1 = null;
-    ObservableList listaProductos;
+    PreparedStatement ps = null;
+    ObservableList<Producto> listaProductos = FXCollections.observableArrayList();
 
-    DatabaseUtil() {
-
+    public DatabaseUtil() {
     }
 
-    public void pruebaDB() {
+    public ObservableList<Producto> anadirLista() {
 
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -55,7 +59,7 @@ public class DatabaseUtil {
                             resultSet1.getString(3), Double.parseDouble(resultSet1.getString(8)),
                             Integer.parseInt(resultSet1.getString(6)), Long.parseLong(resultSet1.getString(1)),
                             resultSet1.getDate(5), resultSet1.getDate(7));
-                    
+                    listaProductos.add(productoAux);
                 } while (resultSet1.next());
             } else {
                 System.out.println("La tabla no tiene datos");
@@ -66,7 +70,37 @@ public class DatabaseUtil {
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return listaProductos;
+    }
 
+    public void insertarProducto(Libro libro) {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conexion = DriverManager.getConnection("jdbc:oracle:thin:noobs/damnoobs@//proyectofinaljfx.ckga19q2gpk5.eu-central-1.rds.amazonaws.com"
+                    + ":8080/LIBRODB");
+            System.out.println("Conectado a base de datos...");
+
+            sentencia = null;
+            resultSet1 = null;
+            sentencia = conexion.createStatement();
+            String insert;
+            insert = "select * from jobs";
+
+            ps = conexion.prepareStatement("insert into jobs values (?,?,?,?,?,?) ");
+            ps.setString(1, String.valueOf(libro.getCodBarras()));
+            ps.setString(2, libro.getNombre());
+            ps.setString(3, libro.getDescription());
+            ps.setString(4, libro.getFoto());
+            if (ps.executeUpdate() != 1) {
+                System.out.println("Error insercion");
+            } else {
+                System.out.println("Fila insertada");
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DatabaseUtil.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- src/view/VistaListadoController.java
+++ src/view/VistaListadoController.java
@@ -6,10 +6,14 @@
 package view;
 
 import controller.GestorLibreria;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import model.Producto;
+import util.DatabaseUtil;
 
 /**
  *
@@ -19,26 +23,48 @@ public class VistaListadoController {
     
     
     private GestorLibreria gestorLibreria;
+    private DatabaseUtil db;
+    private ObservableList<Producto> lista = FXCollections.observableArrayList();
     
-//    VistaListadoController(){
-//        
-//    }
+    @FXML
+    TableView tablaP;
+    @FXML
+    TableColumn codigoColumna;
+    @FXML
+    TableColumn nombreColumna;
+    @FXML
+    TableColumn fechaLanColumna;
+    @FXML
+    TableColumn fechaModColumna;
+    @FXML
+    TableColumn precioColumna;
     
-//    @FXML
-//    private void initialize() {
-//        
-//        
-//    }
+    @FXML
+    private void initialize() {
+        
+        codigoColumna.setCellValueFactory(new PropertyValueFactory<>("codBarras"));
+        nombreColumna.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        fechaLanColumna.setCellValueFactory(new PropertyValueFactory<>("fechaAlta"));
+        fechaModColumna.setCellValueFactory(new PropertyValueFactory<>("fechaModificacion"));
+        precioColumna.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        
+   }
     
     public void setGestorLibreria(GestorLibreria gestorLibreria) {
         
         this.gestorLibreria = gestorLibreria;
+        setListaProductos();
+        
     }
     
     @FXML
     private void nuevo(){
         gestorLibreria.muestraVistaNuevo();
     }
-
     
+    private void setListaProductos(){
+        db = new DatabaseUtil();
+        lista = db.anadirLista();
+        tablaP.setItems(lista);
+    }
 }
